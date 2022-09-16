68e38d7ee6f5ecac375550e0c20f543660bd27e7
Jacob Scott <jacobscott@Jacobs-MacBook-Pro.local>
2022-09-17 00:07:45 +0100

    fix: use quarto.utils.resolvePath() to access R styler script

--- _extensions/r-styler/r-styler.lua
+++ _extensions/r-styler/r-styler.lua
@@ -6,7 +6,11 @@ function CodeBlock (el)
   
   -- Error handling not necessary; if R code won't parse then
   -- knitr will fail and this will never be run
-  el.text = pandoc.pipe("RScript", {"run-styler.R", el.text}, "")
+  el.text = pandoc.pipe(
+    "RScript",
+    {quarto.utils.resolvePath("run-styler.R"), el.text}, 
+    ""
+  )
   
   return el
   
